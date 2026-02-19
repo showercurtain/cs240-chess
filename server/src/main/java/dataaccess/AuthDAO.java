d5b5fd692e13da3cc83f41b5b7a40c60afd5ba44
Sean Cowley <seanjcowley@gmail.com>
2026-02-18 22:23:06 -0700

    Added AuthDAO

---
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -0,0 +1,11 @@
+package dataaccess;
+
+import model.AuthData;
+
+import java.util.UUID;
+
+public interface AuthDAO {
+    void createAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+}
